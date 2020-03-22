7dacf57aa1383d69837dfb8ecc80c6e5cb21386c
hahamimidido <eason@tpe.hyweb.com.tw>
2020-03-22 14:30:18 +0800

    solve flex-box 子項目圖表撐開問題

--- js/customize.js
+++ js/customize.js
@@ -1,16 +1,20 @@
 // customize js
 $(function() {
     $('.header').load("header.htm");
-
     $('.aside').load("aside.htm");
     $('.aside_dark').load("aside_dark.htm");
-
-
     // var resizeChartTimer;
     // $(window).on('resize', function() {
     //     clearTimeout(resizeChartTimer);
     //     resizeChartTimer = setTimeout(function() {
     //         location.reload();
     //     }, 50);
+    // });
+    // $('.dropdown').each(function(index, el) {
+    //     $(this).find('.dropdown-content').hide();
+    //     $(this).find('.dropdown-btn').off().click(function(e) {
+    //     	$(this).next('.dropdown-content').addClass('show');
+    //         e.preventDefault();
+    //     });
     // });
 });
