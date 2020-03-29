8e2c6437686e13fee430004f86d512a283a90545
hahamimidido <eason@tpe.hyweb.com.tw>
2020-03-29 21:07:39 +0800

    add tab

--- js/customize.js
+++ js/customize.js
@@ -3,20 +3,28 @@ $(function() {
     $('.header').load("header.htm");
     $('.aside').load("aside.htm");
     $('.aside_dark').load("aside_dark.htm");
-
-
     // tab
-    var _duration_speed = 400, //設定每張圖轉場動畫速度
-        _vertical_slider_speed = 2000, //設定每張圖停留時間
-        _autoplay_restart = 5000; //設定多久開始啟用autoplay
+    // var _duration_speed = 400, //設定每張圖轉場動畫速度
+    //     _vertical_slider_speed = 2000, //設定每張圖停留時間
+    //     _autoplay_restart = 5000; //設定多久開始啟用autoplay
     $('.tab_slider').each(function(index, el) {
         var _itemLength = $(this).find('.tab_control ul li').length;
-        var pic_index = 0;
-        $(this).find('.tab_control ul li').eq(pic_index).find('a').addClass('active');
-
-
+        var _pic_index = 0;
+        $(this).find('.tab_control ul li').eq(_pic_index).addClass('active');
+        $(this).find('.tab_control ul li')
         // tabcontent
         $(this).find('.tab_content ul li').hide();
-        $(this).find('.tab_content ul li').eq(pic_index).fadeIn();
+        $(this).find('.tab_content ul li').eq(_pic_index).fadeIn();
+        $(this).find('.tab_control ul li').each(function(index, el) {
+            $(this).find('a').off().click(function(e) {
+                var _pic_index = $(this).parent('li').index();
+                $(this).parent('li').addClass('active').siblings('li').removeClass('active');
+                $(this).parents('.tab_control').siblings('.tab_content').find('ul li').fadeIn().eq(_pic_index).addClass('active').siblings('li').removeClass('active').stop(true, true).fadeOut();
+                e.preventDefault();
+            });
+        });
+
+
+
     });
 });
