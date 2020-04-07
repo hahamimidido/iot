f8c123f1438479b022af27394efe70eef7a97fbc
hahamimidido <eason@tpe.hyweb.com.tw>
2020-04-07 10:20:52 +0800

    update

--- js/chart_setting2.js
+++ js/chart_setting2.js
@@ -1,6 +1,6 @@
 $(function() {
     //圖表變數
-    var _Color = ['#0570c2', '#0c9ee7', '#23c4f9', '#ffbc08', '#16aec4', '#03cdc2', '#06e7e4', '#F49F0A'];
+    var _Color = ['#0570c2', '#0c9ee7', '#23c4f9', '#F49F0A', '#03cdc2', '#16aec4', '#06e7e4', '#f6c21d'];
     // Create the chart
     Highcharts.chart('areaChart', {
         chart: {
