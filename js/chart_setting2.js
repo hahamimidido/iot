6732bb7a1ff1150a567e4171be31312c546ad14c
hahamimidido <eason@tpe.hyweb.com.tw>
2020-06-04 11:39:57 +0800

    更新雷達圖

--- js/chart_setting2.js
+++ js/chart_setting2.js
@@ -1046,12 +1046,12 @@ $(function() {
             }
         },
         series: [{
-            type: 'column',
+            type: 'area',
             name: 'Column',
             data: [8, 7, 6, 5, 4, 3, 2, 1],
             pointPlacement: 'between'
         }, {
-            type: 'line',
+            type: 'area',
             name: 'Line',
             data: [1, 2, 3, 4, 5, 6, 7, 8]
         }, {
